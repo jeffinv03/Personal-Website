59dbd072057dfc326f4c21fba748347069a822f5
jeffinv03 <97994153+jeffinv03@users.noreply.github.com>
2022-06-28 12:44:58 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png"; // images/dice1.png
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png"; // images/dice1.png
+
+
+
+var image1 = document.querySelectorAll("img")[0];
+
+var image2 = document.querySelectorAll("img")[1];
+
+
+image1.setAttribute("src", randomDiceImage1);
+
+image2.setAttribute("src", randomDiceImage2);
+
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = " 🏆 Player 1 Wins 🏆";
+} else if ( randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML = " 🏆 Player 2 Wins 🏆";
+} else{
+
+  document.querySelector("h1").innerHTML = " 🤝 Draw 🤝 ";
+
+}
